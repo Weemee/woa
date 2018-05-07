c08af3ca5f76046c544d312a120cb2a128a302d7
Chris Jager <chris.jager.sgs@gmail.com>
2018-05-07 16:01:46 +0200

    Fixed postinstall

--- server/libs/scripts/temp/conf/server.js
+++ server/libs/scripts/temp/conf/server.js
@@ -2,10 +2,15 @@ import {dotEnv} from '../config';
 
 export default {
 	logoutTimer: dotEnv('SERVER_LOGOUT_TIMER', 10000),
-	serverVersion: dotEnv('SERVER_VERSION', '0.0.72'),
-	serverTimers: {
-		name: 'autosave',
-		enabled: dotEnv('SERVER_TIMERS_AUTOSAVE', true),
-		frequency: dotEnv('SERVER_TIMERS_FREQUENCY', 10000),
+	serverVersion: dotEnv('SERVER_VERSION', '0.0.73'),
+	defaultStats: {
+
 	},
+	serverTimers: [
+		{
+			name: 'autosave',
+			enabled: dotEnv('SERVER_TIMERS_AUTOSAVE', true),
+			frequency: dotEnv('SERVER_TIMERS_FREQUENCY', 10000),
+		},
+	],
 };

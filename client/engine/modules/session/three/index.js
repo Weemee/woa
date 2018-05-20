6d2cd59a8bebe651e5ec7f0a95d37a16c0bb9263
Chris Jager <chris.jager.sgs@gmail.com>
2018-05-20 10:35:08 +0200

    3D module rendering

--- client/engine/modules/session/three/index.js
+++ client/engine/modules/session/three/index.js
@@ -1,25 +1,92 @@
 //Base imports
-import React from 'react';
-import {withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 //Custom imports
+import * as THREE from 'three';
 
 //Component class
-class TestThree extends React.Component {
+class TestThree extends Component {
 	constructor(props) {
 		super(props);
+
+		this.start = this.start.bind(this);
+		this.stop = this.stop.bind(this);
+		this.animate = this.animate.bind(this);
+	}
+
+	componentDidMount() {
+		const width = this.mount.clientWidth;
+		const height = this.mount.clientHeight;
+
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera(
+			75,
+			width / height,
+			0.1,
+			1000
+		)
+		const renderer = new THREE.WebGLRenderer({ antialias: true });
+		const geometry = new THREE.SphereGeometry(4, 32, 32);
+		const material = new THREE.MeshBasicMaterial({
+			color: '#FFFF00',
+			wireframe: true
+		})
+		const sphere = new THREE.Mesh(geometry, material);
+
+		camera.position.z = 12;
+		scene.add(sphere);
+		renderer.setClearColor('#000000');
+		renderer.setSize(width, height);
+
+		this.scene = scene;
+		this.camera = camera;
+		this.renderer = renderer;
+		this.material = material;
+		this.sphere = sphere;
+
+		this.mount.appendChild(this.renderer.domElement);
+		this.start();
+	}
+
+	componentWillUnmount() {
+		this.stop();
+		this.mount.removeChild(this.renderer.domElement);
+	}
+
+	start() {
+		if (!this.frameId) {
+			this.frameId = requestAnimationFrame(this.animate);
+		}
+	}
+
+	stop() {
+		cancelAnimationFrame(this.frameId);
+	}
+
+	animate() {
+		this.sphere.rotation.x += 0.01;
+		this.sphere.rotation.y += 0.01;
+
+		this.renderScene();
+		this.frameId = window.requestAnimationFrame(this.animate);
+	}
+
+	renderScene() {
+		this.renderer.render(this.scene, this.camera);
 	}
 
 	render() {
 		return (
-			<div>
-				Testing 3D
-			</div>
-		);
+			<div
+				style={{ width: '720px', height: '720px' }}
+				ref={(mount) => { this.mount = mount }}
+			/>
+		)
 	}
-};
+}
 
 function mapStateToProps(state) {
 	return {
@@ -33,4 +100,4 @@ function mapDispatchToProps(dispatch) {
 	}, dispatch);
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(TestThree));
+export default connect(mapStateToProps, mapDispatchToProps)(TestThree);
